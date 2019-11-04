012a700cb6d3fd25fccc9cfb97e0efe3e43fd2af
zgz567 <3133482702@qq.com>
2019-11-04 23:06:16 +0800

    sss

--- 02.js
+++
@@ -1,8 +0,0 @@
-var http=require("http")
-var url=require("url")
-http.createServer(function(req,res){
-    res.writeHead(200,{"Content-type":"text/html;charset=utf-8"})
-    var obj=url.parse(req.url,true).query
-    console.log(url.parse(req.url,true))
-    res.end("接收"+obj.username)
-}).listen(3000)
--- 06.js
+++
@@ -1,3 +0,0 @@
-const querystring = require('querystring');
-// var obj=querystring.parse("aa==11&bb==22&cc=33"
-console.log(obj)
